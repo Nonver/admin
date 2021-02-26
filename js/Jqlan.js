0bbd1fe5dc79381a08099635755dcea85807e717
绵绵思远道 <66508119+Nonver@users.noreply.github.com>
2021-02-26 19:30:01 +0800

    Add files via upload

---
+++ js/Jqlan.js
@@ -0,0 +1,28 @@
+// Vue
+var app = new Vue({
+	el: '#app',
+	data: {
+		dataName: [],
+		is: '商户管理'
+	},
+	mounted(){
+	    this.init()
+	},
+	methods:{
+	    init(){
+	        console.log('hello,world')
+	    }
+	},
+	created(){
+		var thin = this
+		axios.get('../json/user.json', {
+				params: {}
+			})
+			.then(function(response) {
+				thin.dataName = response.data.data
+			})
+			.catch(function(error) {
+				console.log(error);
+			});
+	}
+})
---
+++ js/lan.js
@@ -0,0 +1,12 @@
+layui.use('element', function() {
+	var element = layui.element;
+
+	//…
+});
+
+// $("#add").on("click", function() {
+// 	layer.open({
+// 		type: 2,
+// 		content: '../home/demo.html' //这里content是一个URL，如果你不想让iframe出现滚动条，你还可以content: ['http://sentsin.com', 'no']
+// 	});
+// });
